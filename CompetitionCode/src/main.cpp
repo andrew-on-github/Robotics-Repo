c003551a3e17837b5301ad39f68e51d21952448a
David Ntim <david.ntim@student.charchrist.com>
2022-02-07 16:00:59 -0500

    Added David Ntim as contributor

--- CompetitionCode/src/main.cpp
+++ CompetitionCode/src/main.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       Andrew Goad, Katie Caldwell, JD Francois                  */
+/*    Author:       Andrew Goad, Katie Caldwell, JD Francois, David Ntim      */
 /*    Created:      Thu Sep 26 2019                                           */
 /*    Description:  Competition Template                                      */
 /*                                                                            */
